291805e7e6154cdf320354ef91f31ea77a7160d3
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-09-14 15:41:37 -0700

    change

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ console.log('id:', id, 'port', port);
 primus.on("connection", function (spark) {
   console.log('connected = ' + id);
   setInterval(function() {
-    spark.write("time: " + new Date().getTime() +" ID: " + id);
+    spark.write("BRANCH time: " + new Date().getTime() +" ID: " + id);
   }, 1000);
 });
